e1b18b10990a5c05fe3a43ab7e8f90c2571cff53
WFM_Anders <Anders.Blankholm@lycatel.com>
2015-04-10 14:45:21 +0100

    Init Commit

---
+++ GCP.Survey/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GCP.Survey.Startup))]
+namespace GCP.Survey
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
